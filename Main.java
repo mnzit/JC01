a1748e59c0c685ea1c87bbedf12ae41458dce063
sandesh <www.sandeshpuri9583@gmail.com>
2022-01-03 03:01:50 -0800

    Multiplication feature added

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     // JVM -> Main Thread -> Main.main
     public static void main(String args[]){
-       MathCommand command = new AddCommand();
+       MathCommand command = new MulCommand();
        command.initParameters(1, 2);
        double result = command.calculate();
        System.out.println(result);
---
+++ MulCommand.java
@@ -0,0 +1,9 @@
+public class MulCommand extends MathCommand{
+
+    @Override
+    public double calculate() {
+        // TODO Auto-generated method stub
+        return i*j;
+    }
+    
+}
