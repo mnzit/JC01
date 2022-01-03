57c36106a8ac3f779134b25b02ae6ffd04658cf0
= <irishbhuju2022@gmail.com>
2022-01-03 17:09:18 +0545

    n

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     // JVM -> Main Thread -> Main.main
     public static void main(String args[]){
-       MathCommand command = new AddCommand();
-       command.initParameters(1, 2);
+       MathCommand command = new SquareRootCommand();
+       command.initParameters(49, 2);
        double result = command.calculate();
        System.out.println(result);
     }
