d8d64fc218b4c18874cbdc87b5e3b2ca78cde8e6
AnishDude <anish.mungrati2012@gmail.com>
2022-01-04 15:28:02 +0545

    method name corrected

--- Basketball.java
+++ Basketball.java
@@ -2,7 +2,7 @@ import javax.swing.event.SwingPropertyChangeSupport;
 
 public class Basketball extends Playable{
     @Override
-     public void Play(){
+     public void play(){
         System.out.println("Playing Basketball");
 
          }
