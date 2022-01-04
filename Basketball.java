67638a8ce89c60d1925a020efdcf3f2665dfce3d
AnishDude <anish.mungrati2012@gmail.com>
2022-01-04 15:22:35 +0545

    Basketball added

---
+++ Basketball.java
@@ -0,0 +1,9 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Basketball extends Playable{
+    @Override
+     public void Play(){
+        System.out.println("Playing Basketball");
+
+         }
+      }
