8417a0ccedd3c9d4eb02548193608fe2c6213517
mnzit <mnzitshakya@gmail.com>
2022-01-10 17:55:44 +0545

    try catch

--- src/com/ggic/jb01/Playable.java
+++
@@ -1,5 +0,0 @@
-package com.ggic.jb01;
-public interface Playable {
-    
-    void play();
-}
--- src/com/ggic/jb01/musicalinstrument/Drum.java
+++
@@ -1,9 +0,0 @@
-package com.ggic.jb01.musicalinstrument;
-
-import com.ggic.jb01.Playable;
-
-public class Drum implements Playable {
-    public void play(){
-        System.out.println("Playing Drum.");
-    }
-}
