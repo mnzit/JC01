73989ebf4551a3aab380cf7c939332703fcddd42
mnzit <mnzitshakya@gmail.com>
2022-01-17 20:30:17 +0545

    unnecessary logic

--- src/com/ggic/jb01/CustomArrayList.java
+++ src/com/ggic/jb01/CustomArrayList.java
@@ -1,31 +1,25 @@
 package com.ggic.jb01;
 
-public class CustomArrayList<T> implements CustomList<T>{
+public class CustomArrayList<T> implements CustomList<T> {
 
     private Object[] elements = new Object[0];
 
     @Override
     public void add(T element) {
-        if (elements.length == 0) {
-            elements = new Object[1];
-            elements[0] = element;
-        }else{
-            int length = elements.length;
-            int newSize = length + 1;
-            Object[] temp = new Object[newSize];
-            // Copying
-            System.arraycopy( elements, 0, temp, 0, elements.length );
-            elements = temp;
-            elements[elements.length-1] = element;
-        }
+        int length = elements.length;
+        int newSize = length + 1;
+        Object[] temp = new Object[newSize];
+        System.arraycopy(elements, 0, temp, 0, elements.length);
+        elements = temp;
+        elements[elements.length - 1] = element;
     }
 
     @Override
     public String toString() {
         String result = "[";
-        for(int i = 0 ; i < elements.length; i++){
+        for (int i = 0; i < elements.length; i++) {
             result += elements[i];
-            if(i != elements.length-1){
+            if (i != elements.length - 1) {
                 result += ", ";
             }
         }
