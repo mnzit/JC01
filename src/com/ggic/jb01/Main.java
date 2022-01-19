e6b7749b17448ce87326452c74615cc6c700efdc
mnzit <mnzitshakya@gmail.com>
2022-01-19 16:30:57 +0545

    workers timing example

--- src/com/ggic/jb01/Main.java
+++ src/com/ggic/jb01/Main.java
@@ -5,16 +5,11 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-
-        Runnable runnable = () -> System.out.println("Thread name: " + Thread.currentThread().getName()+ " worked at: "+new Date().getTime());
-
-        for(int i = 1;i<=10;i++ ) {
-            Thread thread = new Thread(runnable,"worker-" + i);
-            System.out.println("Thread name: worker-"+i+ " created at: "+new Date().getTime());
+        for (int i = 1; i <= 10; i++) {
+            Thread thread = new Thread(new Worker(i), "[worker-" + i + "]");
+            System.out.println("Thread name: worker-" + i + " created at: " + new Date().getTime());
             thread.start();
         }
-
-
     }
 
 }
--- src/com/ggic/jb01/Worker.java
+++ src/com/ggic/jb01/Worker.java
@@ -2,8 +2,20 @@ package com.ggic.jb01;
 
 public class Worker implements Runnable{
 
+    private Integer time;
+
+    public Worker(Integer time) {
+        this.time = time;
+    }
+
     public void work(){
-        System.out.println("Performing his assigned task");
+        System.out.println(Thread.currentThread().getName()+ " performing his assigned task will take time: "+ time+"s");
+        try {
+            Thread.sleep(time.longValue() * 1000);
+        }catch (InterruptedException ex){
+            System.out.println("Ex: "+ ex.getMessage());
+        }
+        System.out.println(Thread.currentThread().getName()+ " finished performing his assigned task");
     }
 
     @Override
