3ec7e3bd9cd47c77f0c47935c67c54cf6cb4714c
mnzit <mnzitshakya@gmail.com>
2022-01-25 17:16:27 +0545

    Notify and Wait

--- src/com/ggic/jb01/Consumer.java
+++ src/com/ggic/jb01/Consumer.java
@@ -1,17 +1,41 @@
 package com.ggic.jb01;
 
-import java.util.stream.IntStream;
+import java.util.List;
 
-public class Consumer implements Runnable{
+public class Consumer implements Runnable {
 
-    private final Resource resource;
+    private final List<Integer> taskContainer;
 
-    public Consumer(Resource resource) {
-        this.resource = resource;
+    public Consumer(List<Integer> taskContainer) {
+        this.taskContainer = taskContainer;
     }
 
     @Override
-    public void run() { //async
-        IntStream.range(1,100).forEach((value) -> resource.consume());
+    public void run() {
+        while (true) {
+            consume();
+        }
+    }
+
+    private void consume() {
+        synchronized (taskContainer) {
+            while (taskContainer.isEmpty()) {
+                try {
+                    System.out.println("Container is empty: " + Thread.currentThread().getName() + " size: " + taskContainer.size());
+                    // consumer is waiting for producer to notify to start consuming
+                    taskContainer.wait();
+                } catch (Exception ex) {
+                }
+            }
+
+            try {
+//                Thread.sleep(1000);
+                int i = taskContainer.remove(taskContainer.size() - 1);
+                System.out.println("consumed: " + i);
+            } catch (Exception ex) {
+
+            }
+            taskContainer.notifyAll();
+        }
     }
 }
--- src/com/ggic/jb01/Product.java
+++
@@ -1,6 +0,0 @@
-package com.ggic.jb01;
-
-public class Product {
-
-    private volatile String name;
-}
