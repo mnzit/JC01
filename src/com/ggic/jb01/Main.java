82b44c3f780de80df52a1e8a8399cb1c8a654bab
mnzit <mnzitshakya@gmail.com>
2022-01-30 17:36:49 +0545

    read and write file

--- src/com/ggic/jb01/Main.java
+++ src/com/ggic/jb01/Main.java
@@ -1,58 +1,35 @@
 package com.ggic.jb01;
 
 import java.io.*;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        File file = new File("folder1" + File.separator + "students.txt");
-        writeStudents(file);
-        readStudents(file);
+        // writeObject();
+        readObject();
     }
 
-
-    public static void writeStudents(File file) {
+    public static void readObject() {
         try {
-            String input = "";
-            while ((input = new Scanner(System.in).nextLine()) != null) {
-                if (!input.equals("Q")) {
-                    FileWriter fileWriter = new FileWriter(file, true);
-                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                    bufferedWriter.append(input);
-                    bufferedWriter.newLine();
-                    bufferedWriter.close();
-                    fileWriter.close();
-                }else{
-                    break;
-                }
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            FileInputStream fileInputStream = new FileInputStream(new File("student.obj"));
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            Student student = (Student) objectInputStream.readObject();
+            System.out.println(student);
+        } catch (Exception exception) {
+            System.out.println("Exception: " + exception);
         }
     }
 
-    public static void readStudents(File file) {
+    public static void writeObject() {
+        Student student = new Student(1L, "Jabir", true);
         try {
-
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            List<Student> students = bufferedReader
-                    .lines()
-                    .filter(s -> s != null && s.trim().length() > 0)
-                    .map(s -> s.split(","))
-                    .filter(s -> s[0] != null && s[1] != null && s[2] != null)
-                    .map(s -> new Student(Long.parseLong(s[0]), s[1], Boolean.parseBoolean(s[2])))
-                    .collect(Collectors.toList());
-
-            fileReader.close();
-            bufferedReader.close();
-            System.out.println(students);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("student.obj"));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(student);
+            objectOutputStream.close();
+            fileOutputStream.close();
+        } catch (Exception exception) {
+            System.out.println("Exception: " + exception);
         }
     }
 }
--- src/com/ggic/jb01/Student.java
+++ src/com/ggic/jb01/Student.java
@@ -1,6 +1,8 @@
 package com.ggic.jb01;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
 
     private Long id;
     private String name;
